1aade9dae584d0a7c7c8e9848ff16609d6b28401
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-07-05 10:50:48 -0700

    Create meta.tsx

---
+++ components/layout/meta.tsx
@@ -0,0 +1,27 @@
+import Head from "next/head";
+
+const Meta = ({ title, keywords, description }) => {
+  return (
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charSet="utf-8" />
+      <meta name="keywords" content={keywords} />
+      <link rel='icon' href='/favicon.ico' />
+      <title>{title}</title>
+      <meta name="title" property="og:title" content={title} />
+      <meta
+        name="description"
+        property="og:description"
+        content={description}
+      />
+    </Head>
+  );
+};
+
+Meta.defaultProps = {
+  title: "Muhammad Ahmad - Full Stack Developer",
+  keywords: "web development, programming, web design",
+  description: "Software Engineer. Lover of web and opensource."
+};
+
+export default Meta;
