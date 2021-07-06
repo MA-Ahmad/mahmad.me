b1f4d9f9a3c48f90e50c7f84d3242265d17a2615
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-07-06 05:57:49 -0700

    added config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,61 @@
-module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+const windmill = require("@windmill/react-ui/config");
+
+module.exports = windmill({
+  purge: {
+    content: ["./pages/**/*.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"]
+  },
+  darkMode: "class", // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"]
+      },
+      typography: theme => ({
+        DEFAULT: {
+          css: {
+            color: theme("colors.gray.900"),
+            code: {
+              color: theme("colors.pink.600")
+            },
+            a: {
+              textDecoration: "none",
+              "&:hover": {
+                textDecoration: "underline"
+              }
+            }
+          }
+        },
+
+        dark: {
+          css: {
+            color: theme("colors.gray.300"),
+            strong: {
+              color: theme("colors.gray.300")
+            },
+            h3: {
+              color: theme("colors.gray.300")
+            },
+            h4: {
+              color: theme("colors.gray.300")
+            },
+            h5: {
+              color: theme("colors.gray.300")
+            },
+            a: {
+              textDecoration: "none",
+              "&:hover": {
+                textDecoration: "underline"
+              }
+            },
+            code: {
+              color: theme("colors.pink.600")
+            }
+          }
+        }
+      })
+    }
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+  variants: { typography: ["dark"] },
+  // eslint-disable-next-line global-require
+  plugins: [require("@tailwindcss/typography")]
+});
