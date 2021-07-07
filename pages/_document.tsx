080ed1b349eeedb32a87573be319d87d2dbf64fa
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-07-07 05:29:16 -0700

    Create _document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta
+            property="og:title"
+            content="Muhammad Ahmad - Full Stack Developer"
+          />
+          <meta property="og:image" content="/logo512.png" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
